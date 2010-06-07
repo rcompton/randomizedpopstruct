e2fb8b0e08e285c192be23b8764963669b560001
ryan <ryan@ryan-laptop2>
2010-06-06 17:30:51 -0700

    did some dumb stuff to convert into 012 format, but cant line up the SNPs yet. Also, some matlab code for the class talk...

---
+++ load_and_proj_custom_data.m
@@ -0,0 +1,51 @@
+
+
+clear all;
+close all;
+clc;
+
+load genotypes_chr22_CEU_r21a_nr.txt.dat;
+load genotypes_chr22_CHB_r21a_nr.txt.dat;
+load genotypes_chr22_JPT_r21a_nr.txt.dat;
+load genotypes_chr22_YRI_r21a_nr.txt.dat;
+
+genotypes_chr22_CEU_r21a_nr_txt = genotypes_chr22_CEU_r21a_nr_txt';
+genotypes_chr22_CHB_r21a_nr_txt = genotypes_chr22_CHB_r21a_nr_txt';
+genotypes_chr22_JPT_r21a_nr_txt = genotypes_chr22_JPT_r21a_nr_txt';
+genotypes_chr22_YRI_r21a_nr_txt = genotypes_chr22_YRI_r21a_nr_txt';
+
+%This gets really stupid...
+[~,n1] = size(genotypes_chr22_CEU_r21a_nr_txt);
+[~,n2] = size(genotypes_chr22_CHB_r21a_nr_txt);
+[~,n3] = size(genotypes_chr22_JPT_r21a_nr_txt);
+[~,n4] = size(genotypes_chr22_YRI_r21a_nr_txt);
+
+n = min([n1,n2,n3,n4]);
+
+genotypes_chr22_CEU_r21a_nr_txt = genotypes_chr22_CEU_r21a_nr_txt(:, 1:n);
+genotypes_chr22_CHB_r21a_nr_txt = genotypes_chr22_CHB_r21a_nr_txt(:, 1:n);
+genotypes_chr22_JPT_r21a_nr_txt = genotypes_chr22_JPT_r21a_nr_txt(:, 1:n);
+genotypes_chr22_YRI_r21a_nr_txt = genotypes_chr22_YRI_r21a_nr_txt(:, 1:n);
+
+big_data = [genotypes_chr22_CEU_r21a_nr_txt; genotypes_chr22_CHB_r21a_nr_txt; genotypes_chr22_JPT_r21a_nr_txt; genotypes_chr22_YRI_r21a_nr_txt];
+clear genotypes_chr22_CEU_r21a_nr_txt
+clear genotypes_chr22_CHB_r21a_nr_txt
+clear genotypes_chr22_JPT_r21a_nr_txt
+clear genotypes_chr22_YRI_r21a_nr_txt
+
+make_projections(big_data);
+
+k = 10;
+
+tic
+[~,s,~] = pca(big_data,k);
+toc
+
+%without 'econ' you crash memory
+tic
+[~,s0,~] = svd(big_data,'econ');
+toc
+
+s
+s0 = s0(1:k, 1:k);
+s0
---
+++ make012.py
@@ -0,0 +1,101 @@
+import string
+
+
+def get_allels(snp):
+    haps = snp.split()
+
+    Af = 0
+    Cf = 0
+    Tf = 0
+    Gf = 0
+    
+    for h in haps:
+        if len(h) != 2:
+            continue
+        else:
+            for ploid in range(1):
+                if h[ploid] == 'A':
+                    Af = Af+1
+                if h[ploid] == 'C':
+                    Cf = Cf+1
+                if h[ploid] == 'T':
+                    Tf = Tf+1
+                if h[ploid] == 'G':
+                    Gf = Gf+1
+
+    majidx = sorted(zip([Af,Cf,Tf,Gf], range(4)), reverse=True) [0][1]
+    minidx = sorted(zip([Af,Cf,Tf,Gf], range(4)), reverse=True) [1][1]
+    
+    if majidx==0:
+        maja = 'A'
+    if majidx==1:
+        maja = 'C'
+    if majidx==2:
+        maja = 'T'
+    if majidx==3:
+        maja = 'G'
+    
+    if minidx==0:
+        mina = 'A'
+    if minidx==1:
+        mina = 'C'
+    if minidx==2:
+        mina = 'T'
+    if minidx==3:
+        mina = 'G'
+    return maja,mina
+
+
+def letters_to_numbers(snp, majorallel, minorallel):
+    haps = snp.split()
+    nums = []
+
+    lexsmall = min(str(majorallel), str(minorallel))
+
+    for hap in haps:
+        if len(hap) != 2:
+            continue
+        else:
+            if hap[0] != hap[1]:
+                nums.append(1)
+            else:
+                if hap[0] == lexsmall or hap[0] == 'N':
+                    nums.append(0)
+                else:
+                    nums.append(2)
+    del hap, haps,lexsmall
+    return nums
+
+def do_a_whole_file(f):
+    outnums = []
+
+    #first line is crap
+    l = f.readline()
+    
+    while l != "":
+        a,b = get_allels(l)
+        nums = letters_to_numbers(l,a,b)
+        outnums.append(nums)
+        del a,b,l,nums
+        l = f.readline()
+
+    outf = open(f.name + ".dat", 'w')
+    for nums in outnums:
+        #gettin' weird
+        outf.writelines(string.join(map(str,nums)) + '\n')
+    
+    return
+
+#Use glob for better software...
+f = open("genotypes_chr22_CEU_r21a_nr.txt")
+do_a_whole_file(f)
+
+f = open("genotypes_chr22_JPT_r21a_nr.txt")
+do_a_whole_file(f)
+
+f = open("genotypes_chr22_CHB_r21a_nr.txt")
+do_a_whole_file(f)
+
+f = open("genotypes_chr22_YRI_r21a_nr.txt")
+do_a_whole_file(f)
+
